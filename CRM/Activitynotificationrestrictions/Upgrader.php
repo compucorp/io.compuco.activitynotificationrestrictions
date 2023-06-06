ace3bfda8103e618682a34696d1d1d8d2ffbfa17
Ayush Mishra <ayush@compuco.io>
2023-06-06 20:12:05 +0530

    FOSFAB-137: Create the extension skeletal

---
+++ CRM/Activitynotificationrestrictions/Upgrader.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Activitynotificationrestrictions_Upgrader extends CRM_Activitynotificationrestrictions_Upgrader_Base {
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# "Activity Notification Restrictions" CiviCRM Extension
